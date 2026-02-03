23b0988ce661ffea155a1db1236244cb4bce4579
Razique089 <abdurrazique651@gmail.com>
2026-02-03 10:19:29 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,5 @@
+#band name generator
+print("welcome to the band name gerenator.")
+city=input("which city did you grow up in?")
+pet=input("what is the name of a pet?")
+print("your band name colud be:"+ city+" "+ pet)
